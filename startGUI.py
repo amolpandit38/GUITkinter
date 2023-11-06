6c472502ceffe4ce7ee7147f846b17cb398c551e
unknown <amol38.ap@gmail.com>
2023-11-06 18:50:26 +0530

    Adding a button format

--- startGUI.py
+++ startGUI.py
@@ -2,11 +2,12 @@ import tkinter as tk
 
 root = tk.Tk()
 
-# this is how you write a label
-label1 = tk.Label(root, text="This is a label")
-label2 = tk.Label(root, text="This is a label")
+def ButtonClicked():
+    label = tk.Label(root, text="Clicked a button!")
+    label.pack()
 
-label1.grid(column=0, row=0)
-label2.grid(column=0, row=1)
+# this is how you write a label
+button = tk.Button(root, text= "A button", state="normal", command=ButtonClicked)
+button.pack()
 
 root.mainloop()
