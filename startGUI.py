0db968f9a2a18569bec341eedc2d281879923f6f
unknown <amol38.ap@gmail.com>
2023-11-06 19:03:59 +0530

    Entry Widget Format

--- startGUI.py
+++ startGUI.py
@@ -3,8 +3,12 @@ import tkinter as tk
 root = tk.Tk()
 
 def ButtonClicked():
-    label = tk.Label(root, text="Clicked a button!")
+    label = tk.Label(root, text=entry.get())
     label.pack()
+
+entry = tk.Entry(root, width=50, bg="white", borderwidth=5)
+entry.pack()
+entry.insert(0, "Default Value")
 
 # this is how you write a label
 button = tk.Button(root, text= "A button", state="normal", command=ButtonClicked)
