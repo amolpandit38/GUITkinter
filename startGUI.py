a5ccbd8e3fe89c38fe9213bb011838257a52a392
unknown <amol38.ap@gmail.com>
2023-11-06 18:36:09 +0530

    Label with grid()

--- startGUI.py
+++ startGUI.py
@@ -3,9 +3,10 @@ import tkinter as tk
 root = tk.Tk()
 
 # this is how you write a label
-label = tk.Label(root, text="This is a label")
-label.pack()
+label1 = tk.Label(root, text="This is a label")
+label2 = tk.Label(root, text="This is a label")
 
-
+label1.grid(column=0, row=0)
+label2.grid(column=0, row=1)
 
 root.mainloop()
