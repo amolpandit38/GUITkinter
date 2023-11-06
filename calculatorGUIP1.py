a50f667d73e3e1a527a9d0bcd548bc3cbda37ce0
unknown <amol38.ap@gmail.com>
2023-11-06 20:46:21 +0530

    Phase 2 clear

--- calculatorGUIP1.py
+++ calculatorGUIP1.py
@@ -2,8 +2,29 @@ import tkinter as tk
 
 root = tk.Tk()
 
+def insertNumber(number):
+    current = valueEntry.get()
+    valueEntry.delete(0, tk.END)
+    valueEntry.insert(0, str(current) + str(number))
+
+def addValues():
+    global firstNumber
+    firstNumberString = valueEntry.get()
+    firstNumber = int(firstNumberString)
+    valueEntry.delete(0, tk.END)
+
+def clearButton():
+    valueEntry.delete(0, tk.END)
+
+def equalButton():
+    secondNumberString = valueEntry.get()
+    valueEntry.delete(0, tk.END)
+    secondNumber = int(secondNumberString)
+    valueEntry.insert(0, firstNumber + secondNumber)
+
 valueEntry = tk.Entry(root, width=35, borderwidth=5)
 valueEntry.grid(column=0, row=0, columnspan=3)
+valueEntry.insert(0, "0")
 
 button1 = tk.Button(root, text = "1", padx=40, pady=20, command = lambda: insertNumber(1))
 button2 = tk.Button(root, text = "2", padx=40, pady=20, command = lambda: insertNumber(2))
@@ -15,6 +36,9 @@ button7 = tk.Button(root, text = "7", padx=40, pady=20, command = lambda: insert
 button8 = tk.Button(root, text = "8", padx=40, pady=20, command = lambda: insertNumber(8))
 button9 = tk.Button(root, text = "9", padx=40, pady=20, command = lambda: insertNumber(9))
 button0 = tk.Button(root, text = "0", padx=40, pady=20, command = lambda: insertNumber(0))
+buttonAdd = tk.Button(root, text = "+", padx=34, pady=20, font=('Arial', 15), command=addValues)
+buttonClear = tk.Button(root, text = "Clear", padx=79, pady=20, command=clearButton)
+buttonEqual = tk.Button(root, text = "=", padx=83.5, pady=20, command=equalButton, font=('Arial', 15))
 
 button1.grid(row=1, column=0)
 button2.grid(row=1, column=1)
@@ -30,8 +54,8 @@ button9.grid(row=3, column=2)
 
 button0.grid(row=4, column=0)
 
-
-def insertNumber(number):
-    valueEntry.insert(0, number)
+buttonAdd.grid(row=5, column=0)
+buttonClear.grid(row=4, column=1, columnspan=2)
+buttonEqual.grid(row=5, column=1, columnspan=2)
 
 root.mainloop()
