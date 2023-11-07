bc489ee0993aed99c848efaa8e4fadfc88d2c51b
unknown <amol38.ap@gmail.com>
2023-11-07 11:40:46 +0530

    image viewer app very basic

---
+++ imageViewAppP2.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+
+
+def forward(number):
+    imageLabel.config(image=imageList[number-1])
+    forwardButton.config(command=lambda:forward(number+1))
+    backButton.config(state='normal', command=lambda:back(number-1))
+    if number == len(imageList):
+        forwardButton.config(state="disabled")
+    
+    
+def back(number):
+    imageLabel.config(image=imageList[number-1])
+    forwardButton.config(command=lambda:forward(number+1), state="normal")
+    backButton.config(state='normal', command=lambda:back(number-1))
+    if number == 1:
+        backButton.config(state="disabled")
+
+
+image1 = ImageTk.PhotoImage(Image.open('myImage.png'))
+image2 = ImageTk.PhotoImage(Image.open('myImage.png'))
+image3 = ImageTk.PhotoImage(Image.open('myImage.png'))
+image4 = ImageTk.PhotoImage(Image.open('myImage.png'))
+image5 = ImageTk.PhotoImage(Image.open('myImage.png'))
+
+imageList = [image1, image2, image3, image4, image5]
+
+imageLabel = tk.Label(root, image=image1, width=600)
+imageLabel.grid(column=0, row=0, columnspan=3)
+
+backButton = tk.Button(root, text="<<", command=back, state="disabled")
+exitButton = tk.Button(root, text="Exit program", command=root.quit)
+forwardButton = tk.Button(root, text=">>", command=lambda: forward(2))
+
+backButton.grid(column=0, row=1)
+exitButton.grid(column=1, row=1)
+forwardButton.grid(column=2, row=1)
+
+root.mainloop()
